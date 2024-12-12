252831f1bc5ac9b518e7cab8418f5d69841bf18d
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-12-13 02:28:48 +0700

    learn multi threading with background threads and queues

---
+++ ContinuedLearning/BackgroundThreadView.swift
@@ -0,0 +1,67 @@
+//
+//  BackgroundThreadView.swift
+//  ContinuedLearning
+//
+//  Created by Ricky Primayuda Putra on 13/12/24.
+//
+
+import SwiftUI
+
+class BackgroundThreadViewModel: ObservableObject {
+    
+    @Published var dataArray: [String] = []
+    
+    func fetchData() {
+        DispatchQueue.global(qos: .background).async {
+            let newData = self.downloadData()
+            
+            print("Check 1: \(Thread.isMainThread)")
+            print("Check 1: \(Thread.current)")
+            
+            DispatchQueue.main.async{
+                self.dataArray = newData
+                print("Check 2: \(Thread.isMainThread)")
+                print("Check 2: \(Thread.current)")
+            }
+        }
+    }
+    
+    private func downloadData() -> [String] {
+        var data: [String] = []
+        
+        for x in 0..<100{
+            data.append("\(x)")
+            print(data)
+        }
+        return data
+    }
+    
+}
+
+struct BackgroundThreadView: View {
+    
+    @StateObject var vm = BackgroundThreadViewModel()
+    
+    var body: some View {
+        ScrollView{
+            LazyVStack(spacing: 10) {
+                Text("Load Data")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .onTapGesture {
+                        vm.fetchData()
+                    }
+                
+                ForEach(vm.dataArray, id: \.self) { item in
+                    Text(item)
+                        .font(.headline)
+                        .foregroundStyle(.red)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    BackgroundThreadView()
+}
